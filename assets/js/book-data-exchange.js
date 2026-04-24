29290abeb5ade9492e88d6e4888e74265255d2e5
marinakotarac <43894269+marinakotarac@users.noreply.github.com>
2026-04-25 00:32:02 +0200

    Update book-data-exchange.js

--- assets/js/book-data-exchange.js
+++ assets/js/book-data-exchange.js
@@ -6,6 +6,16 @@ window.BOOK_DATABASES_EXCHANGE = {
   "englishDescription": "Duplicate copies and books available for exchange with fellow collectors, gathered in a dedicated catalog with contact details.",
   "items": [
     {
+        "localName": "Slovenački jezik",
+        "englishName": "Slovene language",
+        "title": "Mali princ",
+        "wikiUrl": "https://en.wikipedia.org/wiki/Slovene_language",
+        "coverImage": "razmena/naslovna/slovenacki.jpg",
+        "coverThumb": "razmena/naslovna_th/slovenacki.jpg",
+        "firstPageImage": "razmena/prva/slovenacki.jpg",
+        "firstPageThumb": "razmena/prva_th/slovenacki.jpg"
+      },
+    {
         "localName": "Samogitski jezik",
         "englishName": "Samogitian language",
         "title": "Mažāsis Princos",
