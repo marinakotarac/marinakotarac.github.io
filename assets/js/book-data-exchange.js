9b3b800c38c847833529736a8b943d2ec28cb863
marinakotarac <43894269+marinakotarac@users.noreply.github.com>
2026-04-25 21:16:34 +0200

    Update book-data-exchange.js

--- assets/js/book-data-exchange.js
+++ assets/js/book-data-exchange.js
@@ -5,6 +5,16 @@ window.BOOK_DATABASES_EXCHANGE = {
   "description": "Duplikati i primerci namenjeni razmeni sa kolekcionarima, izdvojeni kao poseban katalog sa kontakt podacima.",
   "englishDescription": "Duplicate copies and books available for exchange with fellow collectors, gathered in a dedicated catalog with contact details.",
   "items": [
+     {
+        "localName": "Srpski ćirilica ",
+        "englishName": "Serbian Cyrillic",
+        "title": "Мали принц",
+        "wikiUrl": "https://en.wikipedia.org/wiki/Serbian_Cyrillic_alphabet",
+        "coverImage": "razmena/naslovna/srpski-cirilica_dobrotoljublje.jpg",
+        "coverThumb": "razmena/naslovna_th/srpski-cirilica_dobrotoljublje.jpg",
+        "firstPageImage": "razmena/prva/srpski-cirilica_dobrotoljublje.jpg",
+        "firstPageThumb": "razmena/prva_th/srpski-cirilica_dobrotoljublje.jpg"
+      },
     {
         "localName": "Srpski latinica",
         "englishName": "Serbian Latin",
