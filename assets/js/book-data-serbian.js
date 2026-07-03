52d679c4fbd4b2a50648d79ba87fdb522b4b5711
marinakotarac <43894269+marinakotarac@users.noreply.github.com>
2026-07-03 22:07:48 +0200

    Update book-data-serbian.js

--- assets/js/book-data-serbian.js
+++ assets/js/book-data-serbian.js
@@ -3,6 +3,16 @@ window.BOOK_DATABASES_SERBIAN = {
     "title": "Knjige na srpskom jeziku",
     "description": "Izdanja na srpskom jeziku iz fizicke kolekcije Marine Kotarac, ukljucujuci latinicu, cirilicu i posebna izdanja.",
     "items": [
+        {
+        "localName": "Srpski ćirilica",
+        "englishName": "Serbian Cyrillic",
+        "title": "Мали принц",
+        "wikiUrl": "https://en.wikipedia.org/wiki/Serbian_Cyrillic_alphabet",
+        "coverImage": "naslovna/srpski_cirilica_merlinkompani.jpg",
+        "coverThumb": "naslovna_th/srpski_cirilica_merlinkompani.jpg",
+        "firstPageImage": "prva/srpski_cirilica_merlinkompani.jpg",
+        "firstPageThumb": "prva_th/srpski_cirilica_merlinkompani.jpg"
+      },
           {
         "localName": "Srpski latinica",
         "englishName": "Serbian Latin",
