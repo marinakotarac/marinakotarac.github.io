c1a7d5fb7ca0d518738d97cd5128fa6d670c4e78
marinakotarac <43894269+marinakotarac@users.noreply.github.com>
2026-07-04 20:43:56 +0200

    Update book-data-exchange.js

--- assets/js/book-data-exchange.js
+++ assets/js/book-data-exchange.js
@@ -6,6 +6,16 @@ window.BOOK_DATABASES_EXCHANGE = {
   "englishDescription": "Duplicate copies and books available for exchange with fellow collectors, gathered in a dedicated catalog with contact details.",
   "items": [
      {
+        "localName": "Rumunski jezik",
+        "englishName": "Romanian language",
+        "title": "Micul prinţ",
+        "wikiUrl": "https://en.wikipedia.org/wiki/Romanian_language",
+        "coverImage": "razmena/naslovna/rumunski_2.jpg",
+        "coverThumb": "razmena/naslovna_th/rumunski_2.jpg",
+        "firstPageImage": "razmena/prva/rumunski_2.jpg",
+        "firstPageThumb": "razmena/prva_th/rumunski_2.jpg"
+      },
+     {
         "localName": "Hrvatski jezik",
         "englishName": "Croatian language",
         "title": "Mali princ",
